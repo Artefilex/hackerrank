14cfad85456cca973943eccecac35693829b6b1e
Artefilex <baris.tncdmr@gmail.com>
2023-05-15 22:18:24 +0300

    hackerrank staircase problem  solved

---
+++ algorithms/staircase/index.js
@@ -0,0 +1,12 @@
+function staircase(n) {
+    // Write your code here
+    var line = '';
+    for(let i = 1; i <n +1; i++) {
+        line += Array(n-i).fill(' ').join('')
+        line += Array(i).fill('#').join('')
+        console.log(line)
+        line = ''
+    }
+
+}
+
